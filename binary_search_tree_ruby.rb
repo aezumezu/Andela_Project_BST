116797e009b91754e1cc9cc6d95b65902ab2ded8
aezumezu <aezumezu@gmail.com>
2016-02-24 16:02:13 +0100

    With functions insert and to_s

--- binary_search_tree_ruby.rb
+++ binary_search_tree_ruby.rb
@@ -1,42 +1,64 @@
 # A Binary Search Tree implementation in Ruby
+# Introduced funcions to help insert items to the tree and
+# print the tree structure.
 module AndelaBSTProject
   # The class node to preform actions on a node
   class Node
     attr_accessor :left, :right, :value, :parent
 
     def initialize(v, parent = nil)
+      @value = v
+      @left = EmptyNode.new
+      @right = EmptyNode.new
+      @parent = parent || 'root'
     end
 
     def insert(*v)
-    end
-
-    def get(v)
+      v.each do |val|
+        if @value < val
+          push_right(val, @value)
+        elsif @value > val
+          push_left(val, @value)
+        else
+          false # the value is already present
+        end
+      end
     end
 
     def to_s
-    end
-
-    def to_a
+      ls = left.to_s
+      rs = right.to_s
+      puts "{#{ls} => #{value} <= #{rs} => #{parent}}"
     end
 
     def push_left(v, value)
+      @left.insert(v) || self.left = Node.new(v, value)
     end
 
     def push_right(v, value)
+      @right.insert(v) || self.right = Node.new(v, value)
     end
   end
   # A class to initialize the values of an empty node
   class EmptyNode
-    def to_a
-    end
-
     def get(*)
+      false
     end
 
     def insert(*)
+      false
     end
 
     def to_s
+      'nil'
     end
   end
 end
+
+tree = AndelaBSTProject::Node.new(10)
+tree.insert(15)
+tree.insert(3)
+tree.insert(8)
+tree.insert(35)
+
+puts tree.to_s
