1255920cef71086de1e207f0433c3d2579dc958d
aezumezu <aezumezu@gmail.com>
2016-03-11 08:12:31 +0100

    add a Tree class for creating trees

--- bst_andela.rb
+++ bst_andela.rb
@@ -4,35 +4,33 @@
 module AndelaBSTProject
   # The class node to preform actions on a node
   class Node
-    attr_accessor :left, :right, :value, :parent, :add_c
+    attr_accessor :left, :right, :value, :parent
 
-    def initialize(v, parent = nil)
-      @value = v
+    def initialize(val = nil, parent = nil)
+      @value = val
       @left = EmptyNode.new
       @right = EmptyNode.new
       @parent = parent || 'root'
     end
 
-    def insert(*v)
-      v.each do |val|
-        if @value < val
-          push_right(val, @value)
-        elsif @value > val
-          push_left(val, @value)
+    def insert(key)
+        if @value < key
+          push_right(key, @value)
+        elsif @value > key
+          push_left(key, @value)
         else
           false # the value is already present
         end
-      end
     end
 
     # Returns the value at a node along with its parent
     # and children if they exist.
-    def get(v)
-      if @value > v
-        @left.get(v)
-      elsif @value < v
-        @right.get(v)
-      elsif @value == v
+    def get(key)
+      if @value > key
+        @left.get(key)
+      elsif @value < key
+        @right.get(key)
+      elsif @value == key
         getnodes # the current node is equal to the value
       else
         return nil
@@ -49,77 +47,20 @@ module AndelaBSTProject
       getnodes
     end
 
-    def push_left(v, value)
-      @left.insert(v) || self.left = Node.new(v, value)
+    def push_left(key, value)
+      @left.insert(key) || self.left = Node.new(key, value)
     end
 
-    def push_right(v, value)
-      @right.insert(v) || self.right = Node.new(v, value)
-    end
-
-    def depth_fs(num)
-      found = false
-      my_stack = []
-      track = []
-      count = 0
-      if self.value == num
-        found = true
-        my_stack.clear
-      else
-        my_stack.push(self)
-        last_add = self.value
-        track.push(self.value)
-        count += 1
-        @add_c = true if @add_C == nil
-      end
-      while my_stack.empty? == false
-        current_node = my_stack.delete_at(0)
-        track.delete_at(0)
-        if current_node.left.class != EmptyNode && \
-          current_node.left.value == num
-          found = true #
-          break
-        elsif current_node.right.class != EmptyNode && \
-          current_node.right.value == num
-          found = true #
-          break
-        end
-        puts last_add
-        puts "Count is #{count}"
-        if current_node.left.class != EmptyNode
-          my_stack.push(current_node.left)
-          track.push(current_node.left.value)
-          unless track.include? last_add
-            last_add = current_node.left.value
-            if add_c == true
-              count += 1
-              add_c = false
-            end
-          end
-        end
-        if current_node.right.class != EmptyNode
-          my_stack.push(current_node.right)
-          track.push(current_node.right.value)
-          unless track.include? last_add
-            last_add = current_node.right.value
-            if add_c == true
-              count += 1
-              add_c = false
-            end
-          end
-        end
-        puts "Count is #{count}"
-        puts last_add
-      end
-      if found == true
-        puts "#{num} is #{count} step(s) deep."
-      else
-        puts "#{num} is not in the tree."
-      end
+    def push_right(key, value)
+      @right.insert(key) || self.right = Node.new(key, value)
     end
   end
   # A class to initialize the values of an empty node
   class EmptyNode
+    def get(*)
+      false
+    end
+
     def insert(*)
       false
     end
@@ -128,11 +69,20 @@ module AndelaBSTProject
       'nil'
     end
   end
+  # A class Tree for building the tree objects
+  # accepts an array on definition
+  class Tree < Node
+    def initialize(list)
+      super(list[0])
+      list = list[1..list.length]
+      list.each do |val|
+        insert(val)
+      end
+    end
+  end
+
 end
 
-tree = AndelaBSTProject::Node.new(20)
-tree.insert(10, 35, 5, 47, 8, 9, 38, 42)
+tree = AndelaBSTProject::Tree.new([10, 15, 3, 8, 35, 14, 26])
 
-puts tree.get(9)
-
-#puts tree.get(47)
+puts tree.to_s
